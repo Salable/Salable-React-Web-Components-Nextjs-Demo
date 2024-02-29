05aa4ba66784f0f33c7f44e254cc2f8709a1612b
Sean Cooper <scooper@adaptavist.com>
2024-02-29 17:55:03 +0000

    use test mode data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {defineCustomElements, SalablePricingTable} from "@salable/react-library";
+import {defineCustomElements, SalableCheckout, SalablePricingTable} from "@salable/react-library";
 
 defineCustomElements();
 
@@ -8,14 +8,23 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col gap-8 p-24">
             <h1>Salable React Components Demo</h1>
+            <h2>Pricing Table</h2>
             <SalablePricingTable
-                apiKey="s1tVhRx3WH2jUm2cWTl8g6dbNj7GJ8Zg21XuzEds"
-                uuid="0b8fc07d-6707-491a-ad6e-1828d57fe1cc"
+                apiKey="test_19fda9530350e27eb3948749d511c9d51d09e7c5"
+                uuid="44363fe3-2607-437a-8b66-5375d7a762a2"
                 globalSuccessUrl="https://google.co.uk"
                 globalCancelUrl="https://google.co.uk"
                 globalGranteeId="123"
                 member="456"
                 isCustomPricingTable={false}
+            />
+            <h2>Checkout</h2>
+            <SalableCheckout
+                apiKey="test_19fda9530350e27eb3948749d511c9d51d09e7c5"
+                planUuid="390fe44d-33d9-48fc-9863-eb641e148249"
+                member='example-member-123'
+                granteeId='example-grantee-123'
+                successUrl="https://www.google.com"
             />
         </main>
     );
